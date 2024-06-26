1caae433d1263de98c6e364e5b224324b50e278d
jeffreyai7 <jirukeh@gmail.com>
2024-06-26 14:52:53 +0100

    feat: finished nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,44 @@
 import React from "react";
 import {Link} from "react-router-dom"
 import logo from "../assets/logo.svg"
-
+import cart from "../assets/icon-cart.svg"
+import { ButtonContainer } from "./Button";
+import styled from "styled-components";
 
 function Navbar(){
 
 
     return(
-        <nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
+        <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
                 <Link to="/">
-                <img src={logo} alt="" />
+                <img src={logo} alt="store" className="navbar-brand" />
                 </Link>
-        </nav>
+                <ul className="navbar-nav align-items-center">
+                    <li className="nav-item ml-5">
+                    <Link to = "/" className="nav-link">
+                        Products
+                    </Link>
+                    </li>
+                </ul>
+                <Link to="/cart" className="ml-auto">
+                <ButtonContainer>
+                    <span className="mr-2">
+                        <img src={cart} alt="" />
+                    </span>
+                    my cart
+                </ButtonContainer>
+                </Link>
+        </NavWrapper>
     )
 }
+
+const NavWrapper = styled.nav`
+background: var(--mainBlue);
+.nav-link{
+color:var(--mainWhite)!important;
+font-size: 1.3rem;
+text-transform: capitalize;
+}
+`
 
 export default Navbar;
